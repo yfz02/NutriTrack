1fcf675122bf70411342a57188ed176dac00bdaf
yfz02 <yfzy2002@gmail.com>
2025-08-08 04:03:36 +0200

    Init

---
+++ src/main/java/com/example/nutritrack/NutriTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutritrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nutritrack/NutriTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutritrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
